4b683d6de7c12c06bc4639a93c9d905b67254ff1
philz-cwm6 <phytowardt@gmail.com>
2013-08-06 23:05:04 +0200

    fix missing new line in log

--- recovery.c
+++ recovery.c
@@ -908,7 +908,7 @@ main(int argc, char **argv) {
     // If these fail, there's not really anywhere to complain...
     freopen(TEMPORARY_LOG_FILE, "a", stdout); setbuf(stdout, NULL);
     freopen(TEMPORARY_LOG_FILE, "a", stderr); setbuf(stderr, NULL);
-    printf("Starting recovery on %s (UTC)", ctime(&start));
+    printf("Starting recovery on %s (UTC)\n", ctime(&start));
 
     device_ui_init(&ui_parameters);
     ui_init();
