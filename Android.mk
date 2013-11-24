d0240723cee31fbea82b54765ff101aaa053a2a1
philz-cwm6 <phytowardt@gmail.com>
2013-11-24 23:49:59 +0100

    Add superSU instead of cwm superuser (credits @Chainfire) SuperSU is more compatible with most new stock Samsung ROMs

--- Android.mk
+++ Android.mk
@@ -260,7 +260,7 @@ include $(commands_recovery_local_path)/edify/Android.mk
 include $(commands_recovery_local_path)/updater/Android.mk
 include $(commands_recovery_local_path)/applypatch/Android.mk
 include $(commands_recovery_local_path)/utilities/Android.mk
-include $(commands_recovery_local_path)/su/Android.mk
+include $(commands_recovery_local_path)/su/supersu/Android.mk
 include $(commands_recovery_local_path)/voldclient/Android.mk
 include $(commands_recovery_local_path)/device_images/Android.mk
 
