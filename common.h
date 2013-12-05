45b0030e0cf0fbe9ecc02303018b864578650c19
philz-cwm6 <phytowardt@gmail.com>
2013-12-05 01:08:01 +0100

    Loki: * update to v2.1 to better support LG devices * proper code for recovery: remove orphan variables and simplify error checks * do not lokify after a failed adb install * use original loki format sources for easier future updates

--- common.h
+++ common.h
@@ -52,12 +52,14 @@ void ui_nolog_lines(int lines);
 #ifdef ENABLE_LOKI
 // Toggle for loki support
 extern int loki_support_enabled;
+int loki_check();
 #endif
 
 // Display some header text followed by a menu of items, which appears
 // at the top of the screen (in place of any scrolling ui_print()
 // output, if necessary).
 int ui_start_menu(const char** headers, char** items, int initial_selection);
+
 // Set the menu highlight to the given index, and return it (capped to
 // the range [0..numitems).
 int ui_menu_select(int sel);
