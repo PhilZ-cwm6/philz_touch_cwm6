7e4eb88aef57e53c40cc1fa1acf53c168cff4a57
philz-cwm6 <phytowardt@gmail.com>
2014-04-27 15:38:02 +0200

    6.29.9

--- Android.mk
+++ Android.mk
@@ -74,7 +74,7 @@ endif
 # This should be the same line as upstream to not break makerecoveries.sh
 RECOVERY_VERSION := $(RECOVERY_NAME) v6.0.4.8
 
-PHILZ_BUILD := 6.29.8
+PHILZ_BUILD := 6.29.9
 CWM_BASE_VERSION := $(shell echo $(RECOVERY_VERSION) | cut -d ' ' -f 3)
 LOCAL_CFLAGS += -DCWM_BASE_VERSION="$(CWM_BASE_VERSION)"
 
