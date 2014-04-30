40cac8082cd7236c1c1bd28cb83620b6513fc240
xiaolu <server@163.com>
2014-05-01 00:37:22 +0800

    Dedupe: backup & restore selinux contexts.
    Change-Id: I1f7168e7df0a2f0a27c3627209739c6385da1935

--- dedupe/Android.mk
+++ dedupe/Android.mk
@@ -11,20 +11,20 @@ include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := dedupe.c
-LOCAL_STATIC_LIBRARIES := libcrypto_static libcutils libc
+LOCAL_STATIC_LIBRARIES := libcrypto_static libcutils libc libselinux
 LOCAL_MODULE := libdedupe
 LOCAL_MODULE_TAGS := eng
-LOCAL_C_INCLUDES := external/openssl/include
+LOCAL_C_INCLUDES := external/openssl/include external/libselinux/include
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := driver.c
-LOCAL_STATIC_LIBRARIES := libdedupe libcrypto_static libcutils libc
+LOCAL_STATIC_LIBRARIES := libdedupe libcrypto_static libcutils libc libselinux
 LOCAL_MODULE := utility_dedupe
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_STEM := dedupe
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
-LOCAL_C_INCLUDES := external/openssl/include
+LOCAL_C_INCLUDES := external/openssl/include external/libselinux/include
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
 LOCAL_FORCE_STATIC_EXECUTABLE := true
