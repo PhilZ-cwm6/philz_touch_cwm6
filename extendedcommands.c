b7686e173daad9bbd849146dd31c55569c3b6ef0
philz-cwm6 <phytowardt@gmail.com>
2014-05-31 20:08:19 +0200

    fix f2fs conversion of /data on /data/media devices

--- extendedcommands.c
+++ extendedcommands.c
@@ -727,7 +727,7 @@ int format_device(const char *device, const char *path, const char *fs_type) {
     if (is_data_media_volume_path(path)) {
         return format_unknown_device(NULL, path, NULL);
     }
-    if (strstr(path, "/data") == path && is_data_media()) {
+    if (strstr(path, "/data") == path && is_data_media() && is_data_media_preserved()) {
         return format_unknown_device(NULL, path, NULL);
     }
 
