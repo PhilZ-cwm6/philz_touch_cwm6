0035455f6bc697043f5c5da5bcbbbc2b8fcf3bfa
philz-cwm6 <phytowardt@gmail.com>
2014-06-15 22:07:56 +0200

    merge: "Enable cmdline nandroid backup of vold volumes"

--- recovery.c
+++ recovery.c
@@ -41,8 +41,6 @@
 #include "roots.h"
 #include "recovery_ui.h"
 
-#include "voldclient/voldclient.h"
-
 #include "adb_install.h"
 #include "minadbd/adb.h"
 
@@ -994,6 +992,11 @@ static struct vold_callbacks v_callbacks = {
     .disk_removed = handle_volume_hotswap,
 };
 
+void vold_init() {
+    vold_client_start(&v_callbacks, 0);
+    vold_set_automount(1);
+}
+
 int
 main(int argc, char **argv) {
 
@@ -1069,8 +1072,7 @@ main(int argc, char **argv) {
 
     load_volume_table();
     process_volumes();
-    vold_client_start(&v_callbacks, 0);
-    vold_set_automount(1);
+    vold_init();
     setup_legacy_storage_paths();
     LOGI("Processing arguments.\n");
     ensure_path_mounted(LAST_LOG_FILE);
