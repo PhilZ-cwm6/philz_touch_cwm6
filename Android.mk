82ff3833c3072d9d8e64e5ddb70f487cbea517ed
philz-cwm6 <phytowardt@gmail.com>
2014-06-25 12:28:17 +0200

    6.47.7 / libtouch_gui 1.29

--- Android.mk
+++ Android.mk
@@ -80,7 +80,7 @@ endif
 # This should be the same line as upstream to not break makerecoveries.sh
 RECOVERY_VERSION := $(RECOVERY_NAME) v6.0.4.9
 
-PHILZ_BUILD := 6.47.6
+PHILZ_BUILD := 6.47.7
 CWM_BASE_VERSION := $(shell echo $(RECOVERY_VERSION) | cut -d ' ' -f 3)
 LOCAL_CFLAGS += -DCWM_BASE_VERSION="$(CWM_BASE_VERSION)"
 
