fbdde40e8492c00b540b3333b564a9e2b3441169
Arnav Gupta <championswimmer@aokp.co>
2014-07-06 04:50:03 +0530

    allow using this recovery in a multi-recovery setup
    Change-Id: If327eace92788b4f2a283756204b232186a3b987
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- libtouch_gui/Android.mk
+++ libtouch_gui/Android.mk
@@ -12,7 +12,7 @@ LOCAL_SRC_FILES := touch_gui.c gui_settings.c nandroid_gui.c
 LOCAL_MODULE := libtouch_gui
 
 LOCAL_C_INCLUDES += \
-    bootable/recovery \
+    $(LOCAL_PATH)/.. \
     system/core/fs_mgr/include \
     system/extras/ext4_utils
 
