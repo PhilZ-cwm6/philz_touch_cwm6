14a4b090d9e9127447eb1a53e2a2dc20e1eae69d
philz-cwm6 <phytowardt@gmail.com>
2014-10-13 23:29:53 +0200

    NTFS: merge sources into recovery

--- ntfs-3g/Android.mk
+++ ntfs-3g/Android.mk
@@ -1,26 +1,27 @@
+
 # libfuse lite
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    ../../../external/ntfs-3g/libfuse-lite/fuse.c \
-    ../../../external/ntfs-3g/libfuse-lite/fusermount.c \
-    ../../../external/ntfs-3g/libfuse-lite/fuse_kern_chan.c \
-    ../../../external/ntfs-3g/libfuse-lite/fuse_loop.c \
-    ../../../external/ntfs-3g/libfuse-lite/fuse_lowlevel.c \
-    ../../../external/ntfs-3g/libfuse-lite/fuse_opt.c \
-    ../../../external/ntfs-3g/libfuse-lite/fuse_session.c \
-    ../../../external/ntfs-3g/libfuse-lite/fuse_signals.c \
-    ../../../external/ntfs-3g/libfuse-lite/helper.c \
-    ../../../external/ntfs-3g/libfuse-lite/mount.c \
-    ../../../external/ntfs-3g/libfuse-lite/mount_util.c \
-    ../../../external/ntfs-3g/androidglue/statvfs.c
+    libfuse-lite/fuse.c \
+    libfuse-lite/fusermount.c \
+    libfuse-lite/fuse_kern_chan.c \
+    libfuse-lite/fuse_loop.c \
+    libfuse-lite/fuse_lowlevel.c \
+    libfuse-lite/fuse_opt.c \
+    libfuse-lite/fuse_session.c \
+    libfuse-lite/fuse_signals.c \
+    libfuse-lite/helper.c \
+    libfuse-lite/mount.c \
+    libfuse-lite/mount_util.c \
+    androidglue/statvfs.c
 
+# $(LOCAL_PATH)/.. : do not use hard-coded path (bootable/recovery) to later support different recovery path for variants
 LOCAL_C_INCLUDES := \
-    external/ntfs-3g \
-    external/ntfs-3g/include/fuse-lite \
-    external/ntfs-3g/androidglue/include
+    $(LOCAL_PATH)/../ntfs-3g \
+    $(LOCAL_PATH)/../ntfs-3g/include/fuse-lite \
+    $(LOCAL_PATH)/../ntfs-3g/androidglue/include
 
 LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64  -DHAVE_CONFIG_H
 LOCAL_MODULE := libfuse-lite.recovery
@@ -33,40 +34,40 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    ../../../external/ntfs-3g/libntfs-3g/acls.c \
-    ../../../external/ntfs-3g/libntfs-3g/attrib.c \
-    ../../../external/ntfs-3g/libntfs-3g/attrlist.c \
-    ../../../external/ntfs-3g/libntfs-3g/bitmap.c \
-    ../../../external/ntfs-3g/libntfs-3g/bootsect.c \
-    ../../../external/ntfs-3g/libntfs-3g/cache.c \
-    ../../../external/ntfs-3g/libntfs-3g/collate.c \
-    ../../../external/ntfs-3g/libntfs-3g/compat.c \
-    ../../../external/ntfs-3g/libntfs-3g/compress.c \
-    ../../../external/ntfs-3g/libntfs-3g/debug.c \
-    ../../../external/ntfs-3g/libntfs-3g/device.c \
-    ../../../external/ntfs-3g/libntfs-3g/dir.c \
-    ../../../external/ntfs-3g/libntfs-3g/efs.c \
-    ../../../external/ntfs-3g/libntfs-3g/index.c \
-    ../../../external/ntfs-3g/libntfs-3g/inode.c \
-    ../../../external/ntfs-3g/libntfs-3g/lcnalloc.c \
-    ../../../external/ntfs-3g/libntfs-3g/logfile.c \
-    ../../../external/ntfs-3g/libntfs-3g/logging.c \
-    ../../../external/ntfs-3g/libntfs-3g/mft.c \
-    ../../../external/ntfs-3g/libntfs-3g/misc.c \
-    ../../../external/ntfs-3g/libntfs-3g/mst.c \
-    ../../../external/ntfs-3g/libntfs-3g/object_id.c \
-    ../../../external/ntfs-3g/libntfs-3g/reparse.c \
-    ../../../external/ntfs-3g/libntfs-3g/runlist.c \
-    ../../../external/ntfs-3g/libntfs-3g/security.c \
-    ../../../external/ntfs-3g/libntfs-3g/unistr.c \
-    ../../../external/ntfs-3g/libntfs-3g/unix_io.c \
-    ../../../external/ntfs-3g/libntfs-3g/volume.c \
-    ../../../external/ntfs-3g/libntfs-3g/realpath.c
+    libntfs-3g/acls.c \
+    libntfs-3g/attrib.c \
+    libntfs-3g/attrlist.c \
+    libntfs-3g/bitmap.c \
+    libntfs-3g/bootsect.c \
+    libntfs-3g/cache.c \
+    libntfs-3g/collate.c \
+    libntfs-3g/compat.c \
+    libntfs-3g/compress.c \
+    libntfs-3g/debug.c \
+    libntfs-3g/device.c \
+    libntfs-3g/dir.c \
+    libntfs-3g/efs.c \
+    libntfs-3g/index.c \
+    libntfs-3g/inode.c \
+    libntfs-3g/lcnalloc.c \
+    libntfs-3g/logfile.c \
+    libntfs-3g/logging.c \
+    libntfs-3g/mft.c \
+    libntfs-3g/misc.c \
+    libntfs-3g/mst.c \
+    libntfs-3g/object_id.c \
+    libntfs-3g/reparse.c \
+    libntfs-3g/runlist.c \
+    libntfs-3g/security.c \
+    libntfs-3g/unistr.c \
+    libntfs-3g/unix_io.c \
+    libntfs-3g/volume.c \
+    libntfs-3g/realpath.c
 
 LOCAL_C_INCLUDES := \
-    external/ntfs-3g \
-    external/ntfs-3g/include/fuse-lite \
-    external/ntfs-3g/include/ntfs-3g
+    $(LOCAL_PATH)/../ntfs-3g \
+    $(LOCAL_PATH)/../ntfs-3g/include/fuse-lite \
+    $(LOCAL_PATH)/../ntfs-3g/include/ntfs-3g
 
 LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64  -DHAVE_CONFIG_H
 LOCAL_MODULE := libntfs-3g.recovery
@@ -75,19 +76,19 @@ LOCAL_STATIC_LIBRARIES := libc libcutils
 include $(BUILD_STATIC_LIBRARY)
 
 
-# ntfs-3g
+# ntfs-3g mount binary
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    ../../../external/ntfs-3g/src/ntfs-3g.c \
-    ../../../external/ntfs-3g/src/ntfs-3g_common.c
+    src/ntfs-3g.c \
+    src/ntfs-3g_common.c
 
 LOCAL_C_INCLUDES := \
-    external/ntfs-3g \
-    external/ntfs-3g/include/fuse-lite \
-    external/ntfs-3g/include/ntfs-3g \
-    external/ntfs-3g/androidglue/include \
-    external/ntfs-3g/src
+    $(LOCAL_PATH)/../ntfs-3g \
+    $(LOCAL_PATH)/../ntfs-3g/include/fuse-lite \
+    $(LOCAL_PATH)/../ntfs-3g/include/ntfs-3g \
+    $(LOCAL_PATH)/../ntfs-3g/androidglue/include \
+    $(LOCAL_PATH)/../ntfs-3g/src
 
 LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DHAVE_CONFIG_H
 LOCAL_MODULE := mount.ntfs-3g
@@ -107,22 +108,23 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    ../../../external/ntfs-3g/ntfsprogs/attrdef.c \
-    ../../../external/ntfs-3g/ntfsprogs/boot.c \
-    ../../../external/ntfs-3g/ntfsprogs/sd.c \
-    ../../../external/ntfs-3g/ntfsprogs/mkntfs.c \
-    ../../../external/ntfs-3g/ntfsprogs/utils.c
+    ntfsprogs/attrdef.c \
+    ntfsprogs/boot.c \
+    ntfsprogs/sd.c \
+    ntfsprogs/mkntfs.c \
+    ntfsprogs/utils.c
 
 LOCAL_C_INCLUDES := \
-    external/ntfs-3g \
-    external/ntfs-3g/include/fuse-lite \
-    external/ntfs-3g/include/ntfs-3g \
-    external/ntfs-3g/androidglue/include \
-    external/ntfs-3g/ntfsprogs \
+    $(LOCAL_PATH)/../ntfs-3g \
+    $(LOCAL_PATH)/../ntfs-3g/include/fuse-lite \
+    $(LOCAL_PATH)/../ntfs-3g/include/ntfs-3g \
+    $(LOCAL_PATH)/../ntfs-3g/androidglue/include \
+    $(LOCAL_PATH)/../ntfs-3g/ntfsprogs \
     external/e2fsprogs/lib
 
 LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DHAVE_CONFIG_H
 LOCAL_MODULE := mkntfs.recovery
+
 # need unique moduel name, but binary name should be same as in vold
 # https://github.com/CyanogenMod/android_system_vold
 LOCAL_MODULE_STEM := mkntfs
